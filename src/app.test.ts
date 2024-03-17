9243e3e4434977d3d1ddc685887fab25bb112c4b
Elis Antonio Perez <codewithleader@gmail.com>
2024-03-17 01:22:37 -0500

    test: :test_tube: Test in app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,33 @@
-describe('app.ts', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
+import { ServerApp } from './presentation/server-app';
+describe('Test app.ts', () => {
+  test('should call Server.run with values', async () => {
+    // Arrange
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '5',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    // Act
+    await import('./app');
+
+    // Assert
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination',
+    });
   });
 });
